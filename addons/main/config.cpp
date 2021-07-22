76e460ae2c2b2eb43c13218d536a12d62d0de599
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2021-07-22 20:07:19 +1000

    Changed project name

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,22 +1,22 @@
 class CfgPatches {
-	class intercept_template_plugin { //Change this
-		name = "Intercept Template Plugin"; //Change this
+	class tiglath_pileser {
+		name = "Tiglath Pileser";
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.82;
 		requiredAddons[] = {"intercept_core"};
-		author = "Dedmen"; //Change this
-		authors[] = {"Dedmen"}; //Change this
-		url = "https://github.com/intercept/intercept-plugin-template"; //Change this
+		author = "kyfohatl";
+		authors[] = {"kyfohatl", "shervanator"};
+		url = "https://github.com/kyfohatl/tiglath-pileser";
 		version = "1.0";
 		versionStr = "1.0";
 		versionAr[] = {1,0};
 	};
 };
 class Intercept {
-    class Dedmen { //Change this. It's either the name of your project if you have more than one plugin. Or just your name.
-        class template_plugin { //Change this.
-            pluginName = "template-plugin"; //Change this.
+    class kyfohatl { //Change this. It's either the name of your project if you have more than one plugin. Or just your name.
+        class tiglath_pileser {
+            pluginName = "tiglath-pileser";
         };
     };
 };
