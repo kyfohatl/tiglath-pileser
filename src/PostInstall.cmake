5450d57674e43bfdbc4a90739beceda8c735f386
Shervin Aflatooni <shervinaflatooni@gmail.com>
2021-07-25 16:30:32 +1000

    remove test message

--- src/PostInstall.cmake
+++ src/PostInstall.cmake
@@ -1,5 +1,4 @@
 if (WIN32)
-  message("HELLO LOL")
   set(ARMA_TOOLS_CMD "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3 Tools\\AddonBuilder\\AddonBuilder.exe")
   execute_process(COMMAND ${ARMA_TOOLS_CMD} ${CMAKE_SOURCE_DIR}/addons/main/ ${CMAKE_INSTALL_PREFIX}/../addons)
 endif()
