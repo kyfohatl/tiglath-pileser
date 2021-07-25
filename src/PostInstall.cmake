4f7a987692e1acc3cd9dc9d7a12ef79cc8b1be4a
Shervin Aflatooni <shervinaflatooni@gmail.com>
2021-07-25 16:30:12 +1000

    run pbo builder post install

---
+++ src/PostInstall.cmake
@@ -0,0 +1,5 @@
+if (WIN32)
+  message("HELLO LOL")
+  set(ARMA_TOOLS_CMD "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Arma 3 Tools\\AddonBuilder\\AddonBuilder.exe")
+  execute_process(COMMAND ${ARMA_TOOLS_CMD} ${CMAKE_SOURCE_DIR}/addons/main/ ${CMAKE_INSTALL_PREFIX}/../addons)
+endif()
